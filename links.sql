caf7074d01cc40cdaba87075dc8785da5ed009a4
wwedics <wwedics404@gmail.com>
2023-08-25 20:01:20 +0300

    done

--- links.sql
+++ links.sql
@@ -1,17 +1,27 @@
-DROP TABLE IF EXISTS links;
+-- DROP TABLE IF EXISTS links;
 
-CREATE TABLE links (
-    id SERIAL PRIMARY KEY,
-    url VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255)
-);
+-- CREATE TABLE links (
+--     id SERIAL PRIMARY KEY,
+--     url VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     description VARCHAR(255)
+-- );
 
-INSERT INTO links (id, url, name, description)
-VALUES
-    (1, 'https://kun.uz/', 'Kun.uz', 'Uzbekistan'),
-    (2, 'https://www.youtube.com/', 'YouTube', 'America'),
-    (3, 'https://www.uzmovi.com/', 'Online Films', 'Uzbekistan');
+-- INSERT INTO links (id, url, name, description)
+-- VALUES
+--     (1, 'https://kun.uz/', 'Kun.uz', 'Uzbekistan'),
+--     (2, 'https://www.youtube.com/', 'YouTube', 'America'),
+--     (3, 'https://www.uzmovi.com/', 'Online Films', 'Uzbekistan');
 
 
-SELECT * FROM links;
+-- SELECT * FROM links;
+
+
+
+SELECT 
+    *
+INTO TABLE new_products
+FROM 
+    products;
+
+SELECT * FROM new_products
